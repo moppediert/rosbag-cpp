f171458a89a16da4525e7e9a4c0eb51a1b0d92c4
moppediert <moppediert@gmail.com>
2023-02-09 20:55:14 +0000

    Fix path

--- src/rosbag_cpp/src/rosbag_cpp.cpp
+++ src/rosbag_cpp/src/rosbag_cpp.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <rosbag/bag.h>
 #include <rosbag/query.h>
+#include <rosbag/view.h>
+#include <boost/stacktrace.hpp>
 
-int main() {
+int main()
+{
     rosbag::Bag bag;
-    bag.open("/root/bags/example-3.bag", rosbag::bagmode::Read);
-    rosbag::View view(bag, rosbag::TopicQuery("numbers"));
+    bag.open("/home/jimmy/bags/_2021-08-14-20-37-01.bag", rosbag::bagmode::Read);
+    try
+    {
+        rosbag::View view(bag, rosbag::TopicQuery("/slam/map"));
+    }
+    catch (rosbag::BagIOException)
+    {
+        std::cout << boost::stacktrace::stacktrace();
+    }
     bag.close();
     return 0;
 }
--- src/rosbag_storage/src/bag.cpp
+++ src/rosbag_storage/src/bag.cpp
@@ -57,6 +57,7 @@ namespace rosbag {
 
 Bag::Bag() : encryptor_loader_("rosbag_storage", "rosbag::EncryptorBase")
 {
+    std::cout << "initing bag ..." << std::endl;
     init();
 }
 
@@ -103,6 +104,7 @@ void Bag::init() {
 }
 
 void Bag::open(string const& filename, uint32_t mode) {
+    std::cout << "opening bag ..." << std::endl;
     mode_ = (BagMode) mode;
 
     if (mode_ & bagmode::Append)
